f0230d7f74d452a6859cd357d9eb60a46ef5ce0c
Grizzly21f <106621755+Grizzly21f@users.noreply.github.com>
2023-08-15 22:29:32 +0300

    JS HW 1

---
+++ HW1/main.js
@@ -0,0 +1,45 @@
+//=======TASK1=======//
+
+{
+let s1 ='hello';
+let s2 ='owu';
+let s3 ='com';
+let s4 ='ua';
+
+let n1 =1;
+let n2 = 10;
+let n3 = -999;
+let n4 =123;
+let n5 = 3.14;
+let n6 = 2.7;
+let n7= 16;
+
+let b1 = true;
+let b2 = false;
+
+console.log(`${s1} ${s2} ${s3} ${s4} ${n1} ${n2} ${n3} ${n4} ${n5} ${n6} ${7} ${b1} ${b2}`);
+}
+
+//=======TASK2=======//
+
+let firstName='Taras';
+let middleName ='Mykolayovych';
+let lastName='Firchuk';
+let person=(firstName + ' ' + middleName + ' ' + lastName);
+console.log(person);
+
+//=======TASK3=======//
+
+let n= 100;
+console.log(typeof n);
+let s = '100';
+console.log(typeof s);
+let b = true;
+console.log(typeof b);
+
+//=======TASK4=======//
+let name = prompt('Type your Firstname');
+let middle = prompt('Type your Middlename');
+let last = prompt('Type your LastName');
+let pib =(name + ' ' + middle + ' ' + last);
+console.log(pib);
