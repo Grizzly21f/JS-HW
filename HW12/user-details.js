42dd8e1d2952a80b3453448839868b0f28f23cb8
Grizzly21f <taras.firhuk72@gmail.com>
2023-09-24 18:21:58 +0300

    JS HW 12

--- HW12/user-details.js
+++ HW12/user-details.js
@@ -7,15 +7,24 @@ const ul = document.createElement('ul');
 fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
     .then(response => response.json())
     .then(data => {
-        const mainUserInfoLi = document.createElement('li');
-        mainUserInfoLi.innerText = `id: ${data.id}, name: ${data.name}, username: ${data.username}, email: ${data.email}`;
+        const userExplorer = (object) => {
 
-        userAddressLi = document.createElement('li');
-        userAddressLi.innerText = `street: ${data.address.street}, suite: ${data.address.suite}, City: ${data.address.city} ,Zipcode: ${data.address.zipcode} ,${data.address.geo.lat},
-         ${data.address.geo.lng} , Phone: ${data.phone}, Web: ${data.website},CmpName: ${data.company.name}, ${data.company.catchPhrase},${data.company.bs}`
+            for (const item in object) {
+                if (typeof object[item] === 'object') {
+                    const li = document.createElement('li');
+                    li.innerText = `-----${item}-----`;
+                    ul.appendChild(li);
+                    userExplorer(object[item]);
+                } else {
+                    const li = document.createElement('li');
+                    li.innerText = `${item}: ${object[item]}`;
+                    ul.appendChild(li);
+                }
 
-        ul.append(mainUserInfoLi,userAddressLi);
-        userWrapper.appendChild(ul)
+            }
+        }
+        userExplorer(data);
+        userWrapper.appendChild(ul);
     })
 
 
